a8ffcba7f2e56181cfebc9257fe7c48aa036bc73
George Richardson <georgerichardson@posteo.net>
2020-05-06 19:31:27 +0100

    add camel_to_snake and fetch

---
+++ sdg_mapping/utils/misc_utils.py
@@ -0,0 +1,17 @@
+import re
+
+def camel_to_snake(name):
+    '''Convert lowerCamelCase or upperCamelCase to snake_case
+    Args:
+        name (str): lowerCamelCase or upperCamelCase word.
+    Returns:
+        _name (str): snake_case word.
+    '''
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+def fetch(url, fout):
+    '''fetch
+    Fetches a file from the web and saves to a location on disk.
+    '''
+    urllib.request.urlretrieve(url, fout)
