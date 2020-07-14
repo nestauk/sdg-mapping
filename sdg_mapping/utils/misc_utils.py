8b402cec26d66395ac8f8c219b614c8fcd86a9b0
George Richardson <georgerichardson@posteo.net>
2020-07-14 16:33:25 +0100

    load sdg index data from 2016, 2017, 2018 and 2020

--- sdg_mapping/utils/misc_utils.py
+++ sdg_mapping/utils/misc_utils.py
@@ -16,3 +16,20 @@ def fetch(url, fout):
     Fetches a file from the web and saves to a location on disk.
     '''
     urlretrieve(url, fout)
+
+def _decode_json_int(o):
+    """_decode_json_int
+    Loads integers in a json as int. Pass in as parameter `object_hook` for
+    `json.load`.
+    """
+    if isinstance(o, str):
+        try:
+            return int(o)
+        except ValueError:
+            return o
+    elif isinstance(o, dict):
+        return {_decode_json_int(k): _decode_json_int(v) for k, v in o.items()}
+    elif isinstance(o, list):
+        return [_decode_json_int(v) for v in o]
+    else:
+        return o
